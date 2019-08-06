713da231987ad77421881751ce24935d2dbd61e1
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-06 03:00:09 -0400

    minor changes to test nodemon functionality

--- app.js
+++ app.js
@@ -5,6 +5,4 @@ const getNotes = require('./notes.js')
 const notes = getNotes();
 console.log(notes)
 
-console.log(chalk.green("Success!"))
-console.log(chalk.blue.bold("bolded text"))
-console.log(chalk.inverse.red("inverted text"))
+console.log(chalk.red.inverse("Error!"))
